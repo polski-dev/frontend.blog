47526aad69bf2b593c7a193aee9ea0f1084e0376
Paweł Niedźwiecki <hello@uxu.pl>
2022-02-04 09:04:51 +0100

    i update api

--- src/pages/api/articles/[q].tsx
+++ src/pages/api/articles/[q].tsx
@@ -5,7 +5,7 @@ export default async function handler(req: any, res: any) {
   const { q } = req.query;
 
   const response = await fetch(
-    `${process.env.URL_API}/api/article?pagination[page]=${q}&pagination[pageSize]=10&populate=cover&populate=comments&populate=grades&populate=tags&populate=author&filters[waitingroom][$eq]=false`
+    `${process.env.URL_API}/api/article?pagination[page]=${q}&pagination[pageSize]=10&populate=cover&populate=comments&populate=grades&populate=tags&populate=author&populate[1]=author.avatar&filters[waitingroom][$eq]=false`
   );
   const data = response.json();
   data.then((response) => res.status(200).json(response));
