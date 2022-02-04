38dbef5fb6e218801771c726c3fd9742cd67496c
Paweł Niedźwiecki <hello@uxu.pl>
2022-02-04 15:03:56 +0100

    i added handling errors in home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,17 +3,16 @@ import { ShortArticle } from "components/templates/section/component.section.ind
 import { MenuPrimary, MenuTable } from "components/templates/menu/component.menu.index";
 import { Container, Row, Col } from "components/orgamis/flexboxgrid/index.flexboxgrid";
 
-const Home: NextPage = ({ tags, aticle }: any) => {
+const Home: NextPage = ({ tags, videos, aticles }: any) => {
   return (
     <Container>
       <Row>
-        <MenuPrimary tags={tags.data} />
+        {tags?.err ? <>Nie mogę wczytać komponentu spróbuj za 1h</> : <MenuPrimary tags={tags.data} />}
+
         <Col xs={12} md={9} xl={8}>
-          <ShortArticle data={aticle.data} slug="a" title="Blog" />
+          {aticles?.err ? <>Nie mogę wczytać komponentu spróbuj za 1h</> : <ShortArticle data={aticles.data} slug="a" title="Blog" />}
         </Col>
-        <Col xl={2}>
-          <MenuTable />
-        </Col>
+        <Col xl={2}>{videos?.err ? <>Nie mogę wczytać komponentu spróbuj za 1h</> : <MenuTable />}</Col>
       </Row>
     </Container>
   );
@@ -22,22 +21,21 @@ const Home: NextPage = ({ tags, aticle }: any) => {
 export async function getStaticProps() {
   // top tags
   const tagsResponse = await fetch(`https://www.polski.dev/api/tags/1`);
-  const tags = await tagsResponse.json();
-
-  // aticle
-  const aticleResponse = await fetch(`https://www.polski.dev/api/articles/1`);
-  const aticle = await aticleResponse.json();
+  const tags = await tagsResponse.json().catch((err) => ({ err: true }));
 
   // video
-  const videoResponse = await fetch(`http://localhost:3000/api/video/1`);
-  const video = await aticleResponse.json();
+  const videosResponse = await fetch(`https://www.polski.dev/api/videos/1`);
+  const videos = await videosResponse.json().catch((err) => ({ err: true }));
 
-  console.log(video);
+  // aticle
+  const aticlesResponse = await fetch(`https://www.polski.dev/api/articles/1`);
+  const aticles = await aticlesResponse.json().catch((err) => ({ err: true }));
 
   return {
     props: {
       tags,
-      aticle,
+      videos,
+      aticles,
     },
   };
 }
