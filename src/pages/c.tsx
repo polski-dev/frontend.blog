db1971a1fa7ea80463ea04d8a81ce9426d73ccd0
Paweł Niedźwiecki <hello@uxu.pl>
2022-02-09 21:19:31 +0100

    i update contact

--- src/pages/c.tsx
+++ src/pages/c.tsx
@@ -1,17 +1,43 @@
 import Head from "next/head";
-import type { NextPage } from "next";
-import { MenuPrimary, MenuTable } from "components/templates/menu/component.menu.index";
-import { Container, Row, Col } from "components/orgamis/flexboxgrid/index.flexboxgrid";
+import { NextPage } from "next";
+import { useContext, useEffect } from "react";
+import { RootState } from "store/store.index";
 
-const Contact: NextPage = ({ tags, aticles }: any) => {
+import { MenuContext } from "providers/providers.menu";
+import { useSelector, useDispatch } from "react-redux";
+import { addTag } from "store/slice/tag/store.slice.tag";
+import { addContentAllHome } from "store/slice/content/store.slice.content";
+import { Container, Row, Col } from "components/orgamis/flexboxgrid/index.flexboxgrid";
+import { MenuPrimary, MenuTable } from "components/templates/menu/component.menu.index";
+
+const Contact: NextPage = ({ tag, content }: any) => {
+  const dispatch = useDispatch();
+  const { setModeMenu } = useContext(MenuContext);
+  const story = useSelector((state: RootState) => state);
+
+  useEffect(() => setModeMenu("display"), [setModeMenu]);
+  useEffect(() => {
+    !story.tag.all.home.data.length && !!tag.data.length && dispatch(addTag({ all: tag }));
+    !story.content.all.home.data.length && !!content.all.data && dispatch(addContentAllHome(content));
+  }, [dispatch, content, tag, story]);
+
+  if (content?.err || tag?.err) return <>Mamy problem z wczytaniem tego widoku spróbuj za 1h</>;
+
   return (
     <>
       <Head>
-        <title>Kontakt | POLSKI.DEV 👩‍💻👨‍💻</title>
+        <title>Blog | POLSKI.DEV 👩‍💻👨‍💻</title>
       </Head>
       <Container>
         <Row>
-          {/* <MenuPrimary tags={tags.data} /> */}
+          <MenuPrimary
+            title="Filtruj"
+            data={[
+              { slug: "/", title: "Wszystko", quantity: content.all.meta.pagination.total },
+              { slug: "/a", title: "Artykuły", quantity: content.article.meta.pagination.total },
+              { slug: "/v", title: "Video", quantity: content.video.meta.pagination.total },
+            ]}
+          />
           <Col xs={12} md={9} xl={8}>
             <h5 style={{ marginTop: "3rem", paddingBottom: "1.5rem" }}>Kontakt</h5>
             <div style={{ padding: "3rem 1.5rem", background: "#2D2E30", borderRadius: "0.6rem", display: "flex", alignItems: "center", justifyContent: "center", height: "80vh", marginBottom: "3rem" }}>
@@ -23,7 +49,7 @@ const Contact: NextPage = ({ tags, aticles }: any) => {
               </div>
             </div>
           </Col>
-          {/* <MenuTable data={aticles.data} title="blog" type="article" /> */}
+          <MenuTable type="video" />
         </Row>
       </Container>
     </>
@@ -32,17 +58,17 @@ const Contact: NextPage = ({ tags, aticles }: any) => {
 
 export async function getStaticProps() {
   // tag
-  const tagsResponse = await fetch(`https://www.polski.dev/api/tag/1`);
-  const tags = await tagsResponse.json().catch((err) => ({ err: true }));
+  const tagResponse = await fetch(`https://www.polski.dev/api/tag/1`);
+  const tag = await tagResponse.json().catch((err) => ({ err: true }));
 
   // content
   const contentResponse = await fetch(`https://www.polski.dev/api/content/1`);
-  const aticles = await contentResponse.json().catch((err) => ({ err: true }));
+  const content = await contentResponse.json().catch((err) => ({ err: true }));
 
   return {
     props: {
-      tags,
-      aticles,
+      tag,
+      content,
     },
   };
 }
