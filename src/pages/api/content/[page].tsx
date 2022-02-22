4fdca37c1745efd07906a05cd0a9d4d9b8bca3af
Paweł Niedźwiecki <hello@uxu.pl>
2022-02-22 17:54:42 +0100

    update

--- src/pages/api/content/[page].tsx
+++ src/pages/api/content/[page].tsx
@@ -1,11 +1,11 @@
-import { contentShortGetPreview, contentInitialState } from "database/database.graphQL.index";
+import { contentShortGetPreview, contentShortInitialState } from "database/database.graphQL.index";
 
 export default async function contentAPI(req: any, res: any): Promise<void> {
   const { page } = req.query;
   const { waitingroom = false } = req.body;
 
   // i check page number
-  if (parseInt(page) < 0) res.status(200).json(contentInitialState);
+  if (parseInt(page) < 0) res.status(200).json(contentShortInitialState);
   else if (parseInt(page) === 0 ? false : !parseInt(page)) res.status(500).json({ err: "wrong page number" });
 
   res.status(200).json(await contentShortGetPreview(parseInt(page), waitingroom));
