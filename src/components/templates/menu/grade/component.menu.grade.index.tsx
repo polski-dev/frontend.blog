2638c0d4eeb618f8c8e31c4294b3e2bd2c734a6d
Paweł Niedźwiecki <hello@uxu.pl>
2022-03-03 21:11:26 +0100

    update

--- src/components/templates/menu/grade/component.menu.grade.index.tsx
+++ src/components/templates/menu/grade/component.menu.grade.index.tsx
@@ -2,15 +2,24 @@ import { useEffect } from "react";
 import Wow from "assets/icon/wow.svg";
 import Wrr from "assets/icon/wrr.svg";
 import Eye from "assets/icon/eye.svg";
+import { useRouter } from "next/router";
 import Best from "assets/icon/best.svg";
+import { useSession } from "next-auth/react";
+import { RootState } from "store/store.index";
 import Comment from "assets/icon/comment.svg";
 import { polyfill } from "smoothscroll-polyfill";
+import { useSelector, useDispatch } from "react-redux";
 import { MenuGradeType } from "./component.menu.grade.type";
+import { addGrade } from "store/slice/grade/store.slice.grade";
 import { Button } from "components/atoms/button/component.button.index";
 import { BoxMenu, BoxContent, List, Item, Quantity, Title } from "./component.menu.grade.style";
 
 export default function MenuGrade({ grade, views, comments, idArticle }: MenuGradeType): JSX.Element {
+  const { data } = useSession();
+  const router = useRouter();
+  const dispatch = useDispatch();
   useEffect(() => polyfill(), []);
+  const store = useSelector((state: RootState) => state);
 
   const scrollBoxComment = () => {
     if (!!idArticle) {
@@ -19,6 +28,36 @@ export default function MenuGrade({ grade, views, comments, idArticle }: MenuGra
       return boxComments?.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
     }
   };
+
+  useEffect(() => {
+    const localStorage = window.localStorage;
+    console.log(localStorage.grade);
+    console.log(store);
+    if (!!data) {
+      if (!!store.grade.data.grade) {
+      } else {
+      }
+    } else if (!!store.grade.data.grade) {
+      localStorage.setItem("grade", JSON.stringify({ ...store.grade.data, slug: router.asPath }));
+      console.log(JSON.stringify({ ...store.grade.data, slug: router.asPath }));
+      // router.prefetch("/auth/signin");
+    }
+  }, [store, data, router]);
+
+  useEffect(() => {
+    // console.log(data);
+    // console.log(store);
+    setTimeout(() => {
+      console.log("ok");
+      dispatch(
+        addGrade({
+          grade: "wow",
+          id: 1,
+          type: "article",
+        })
+      );
+    }, 1000);
+  }, [store, dispatch, data]);
 
   return (
     <BoxMenu>
--- src/components/templates/section/sessionInfo/component.section.sessionInfo.index.tsx
+++ src/components/templates/section/sessionInfo/component.section.sessionInfo.index.tsx
@@ -2,18 +2,20 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 import { useState, useEffect } from "react";
 import { useSession } from "next-auth/react";
+import { RootState } from "store/store.index";
+import { useSelector, useDispatch } from "react-redux";
 import { Section, BoxContent, BoxInfo, BoxError } from "./component.section.sessionInfo.style";
 
 export default function SectionSessionInfo({ users }: { users: number }) {
-  const [sec, setSec] = useState(5);
   const router = useRouter();
   const { data: session } = useSession();
+  const store = useSelector((state: RootState) => state);
 
   useEffect(() => {
+    console.log(store);
     const stoper: ReturnType<typeof setInterval> = setInterval(async () => {
-      if (sec === 0) await router.push("/");
-      setSec(sec - 1);
-    }, 1000);
+      await router.push("/");
+    }, 3000);
 
     return () => clearInterval(stoper);
   }, [sec, router]);
@@ -24,16 +26,11 @@ export default function SectionSessionInfo({ users }: { users: number }) {
       <BoxContent>
         {!!session ? (
           <BoxInfo>
-            {session?.user?.name} udało Ci się potwierdzić że jesteś jedną z {users} niesamiwitych osób, za {sec} sek zostaniesz przekierowany na
-            <span>
-              <Link href="/">
-                <a> strone główną</a>
-              </Link>
-            </span>
+            {session?.user?.name} udało Ci się potwierdzić że jesteś jedną z {users} niesamiwitych osób, zaraz zostaniesz przekierowany
           </BoxInfo>
         ) : (
           <BoxError>
-            Upss... Coś poszło nie tak :( za {sec} sek zostaniesz przekierowany na
+            Upss... Coś poszło nie tak :( zaraz zostaniesz przekierowany na
             <span>
               <Link href="/">
                 <a> stronę główną </a>
