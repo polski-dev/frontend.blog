fa4c42951e6ef85dab2606f63c22940a20897868
Paweł Niedźwiecki <hello@uxu.pl>
2022-04-02 01:34:04 +0200

    i added hooks for himself

--- src/database/user/type/database.userHimselfDataEditPublic.type.tsx
+++ src/database/user/type/database.userHimselfDataEditPublic.type.tsx
@@ -1,23 +1,14 @@
 export interface UserHimselfDataEditPublicType {
   data?: {
-    id: number;
-    username: string;
-    email: string;
-    blocked: boolean;
-    views: number;
-    createdAt: Date;
-    updatedAt: Date;
-    about: string;
-    website: string;
-    youtube: string;
-    instagram: string;
-    tiktok: string;
-    github: string;
-    city: string;
-    country: string;
-    avatar: {
-      url: string;
-    };
+    username?: string;
+    about?: string;
+    website?: string;
+    youtube?: string;
+    instagram?: string;
+    tiktok?: string;
+    github?: string;
+    city?: string;
+    country?: string;
   };
   error?: {
     status: number;
--- src/hooks/hooks.useHimself.tsx
+++ src/hooks/hooks.useHimself.tsx
@@ -19,6 +19,57 @@ export default function useHimself({ id, slug }: { id: number; slug?: string }) 
   const router: NextRouter = useRouter();
   const { data: session } = useSession();
   const { addCallBackURL } = useCallBackURL();
+  const [userHimselfData, setUserHimselfData] = useState(userHimselfDataInitialState);
+  const [userHimselfDataEditEmail, setUserHimselfDataEditEmail] = useState(userHimselfDataEditEmailInitialState);
+  const [userHimselfDataEditPassword, setUserHimselfDataEditPassword] = useState(userHimselfDataEditPasswordInitialState);
+  const [userHimselfDataEditPublic, setUserHimselfDataEditPublic] = useState(userHimselfDataEditPublicInitialState);
+  const [userHimselfDelete, setUserHimselfDelete] = useState(userHimselfDeleteInitialState);
 
-  return {};
+  useEffect(() => {
+    if (!!session?.jwt)
+      (async () => {
+        const data = await userHimselfDataGetPreview(typeof session?.jwt === "string" ? session?.jwt : "");
+        setUserHimselfData(data);
+      })();
+  }, [userHimselfData, session, userHimselfDataEditEmail, userHimselfDataEditPassword, userHimselfDataEditPublic, userHimselfDelete]);
+
+  const userHimselfDataEditEmailGet = async (email: string) => {
+    const editEmailData = await userHimselfDataEditEmailGetPreview(typeof session?.jwt === "string" ? session?.jwt : "", email);
+    setUserHimselfDataEditEmail(editEmailData);
+    return editEmailData;
+  };
+
+  const userHimselfDataEditPasswordGet = async (password: string) => {
+    const editPasswordData = await userHimselfDataEditPasswordGetPreview(typeof session?.jwt === "string" ? session?.jwt : "", password);
+    setUserHimselfDataEditPassword(editPasswordData);
+    return editPasswordData;
+  };
+
+  const userHimselfDataEditPublicGet = async ({
+    data,
+  }: {
+    data: {
+      username?: string;
+      about?: string;
+      website?: string;
+      youtube?: string;
+      instagram?: string;
+      tiktok?: string;
+      github?: string;
+      city?: string;
+      country?: string;
+    };
+  }) => {
+    const publicData = await userHimselfDataEditPublicGetPreview(typeof session?.jwt === "string" ? session?.jwt : "", data);
+    setUserHimselfDataEditPublic(publicData);
+    return publicData;
+  };
+
+  const userHimselfDeleteGet = async () => {
+    const deleteData = await userHimselfDeleteTypeGetPreview(typeof session?.jwt === "string" ? session?.jwt : "");
+    setUserHimselfDelete(deleteData);
+    return deleteData;
+  };
+
+  return { userHimselfData, userHimselfDataEditEmailGet, userHimselfDataEditPasswordGet, userHimselfDataEditPublicGet, userHimselfDeleteGet };
 }
