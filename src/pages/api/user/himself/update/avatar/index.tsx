52fed510aefe9c5640762384d13debf637c1d166
Paweł Niedźwiecki <hello@uxu.pl>
2022-04-14 22:35:34 +0200

    i added CreateMessageErr

--- src/pages/api/user/himself/update/avatar/index.tsx
+++ src/pages/api/user/himself/update/avatar/index.tsx
@@ -1,43 +1,138 @@
-import formidable from "formidable";
-import FormData from "form-data";
 import fs from "fs";
-
+import formidable from "formidable";
+const FormData = require("form-data");
 import type { NextApiRequest, NextApiResponse } from "next";
+
 export const config = {
   api: {
     bodyParser: false,
   },
 };
 
+const errCreateMsg: ({ status, name, message, path }: { status: number; name: string; path: string[]; message: string }) => {
+  error: {
+    status: number;
+    name: string;
+    message: string;
+    details: {
+      errors: [
+        {
+          path: string[];
+          message: string;
+          name: string;
+        }
+      ];
+    };
+  };
+} = ({
+  status,
+  name,
+  message,
+  path,
+}: {
+  status: number;
+  name: string;
+  path: string[];
+  message: string;
+}): {
+  error: {
+    status: number;
+    name: string;
+    message: string;
+    details: {
+      errors: [
+        {
+          path: string[];
+          message: string;
+          name: string;
+        }
+      ];
+    };
+  };
+} => {
+  return {
+    error: {
+      status,
+      name,
+      message,
+      details: {
+        errors: [
+          {
+            path: [],
+            message,
+            name,
+          },
+        ],
+      },
+    },
+  };
+};
+
 const post = async (req: NextApiRequest, res: NextApiResponse) => {
-  const form = new formidable.IncomingForm();
-  form.parse(req, async function (err, fields, files) {
-    await saveFile(files.avatar).then((d) => {
-      console.log(d);
-      res.status(200).json({ ok: "ok" });
-    });
+  const IncomingDataFromForm = new formidable.IncomingForm();
+
+  IncomingDataFromForm.parse(req, async function (err: any, fields, files) {
+    if (err) return res.status(400).json({ err: true });
+
+    const file: any = typeof files?.avatar === "object" ? files.avatar : null;
+
+    !!file && (await saveFile(file));
+    res.status(200).json({ ok: "ok" });
   });
 };
 
-const saveFile = async (file: any) => {
-  // const data = fs.readFileSync(file.filepath);
-  const form = new FormData();
-  form.append("avatar", fs.createReadStream(file.filepath));
-  form.append("name", "face.png");
+const saveFile = async (file: { filepath: string; originalFilename: string }) => {
+  fs.readFile(`${__dirname}/${file.originalFilename}`, (err) => {
+    console.log(err);
+  });
+  const data: Buffer = fs.readFileSync(file.filepath);
 
-  const uploadResponse = await fetch("http://localhost:1337/api/user/himself/data/changeavatar", {
+  fs.writeFile(`${__dirname}/${file.originalFilename}`, data, function (err) {
+    if (err) throw err;
+  });
+
+  const form: any = new FormData();
+  form.append("avatar", fs.createReadStream(`${__dirname}/${file.originalFilename}`));
+
+  fetch("http://localhost:1337/api/user/himself/data/changeavatar", {
     method: "POST",
     headers: {
       Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTEsImlhdCI6MTY0OTI2OTYxNywiZXhwIjoxNjUxODYxNjE3fQ.wf8qFWd5_KgAtskc8AOQ5Fa3xkvjeSaAVyORYJRDmpU",
     },
     body: form,
   })
-    .then((response) => response.json().then((d) => console.log(d)))
-    .catch((err) => err);
+    .then((response) =>
+      response.json().then((d) => {
+        fs.unlink(`${__dirname}/${file.originalFilename}`, (err) => {
+          console.log(err);
+        });
+        return console.log(d);
+      })
+    )
+    .catch((err) => {
+      console.error(err);
+    });
 
   return;
 };
 
 export default async function userHimselfAvatarChange(req: NextApiRequest, res: NextApiResponse) {
-  req.method === "POST" ? post(req, res) : res.status(404).json({ err: "l" });
+  req.method === "POST"
+    ? post(req, res)
+    : res.status(404).json({
+        error: {
+          status: 404,
+          name: "Not Found",
+          message: `Not Found`,
+          details: {
+            errors: [
+              {
+                path: [],
+                message: `Not Found`,
+                name: "Not Found",
+              },
+            ],
+          },
+        },
+      });
 }
