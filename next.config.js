2b235f7dd0edae4a59174c1e6ef6a1b9d7fac199
Paweł Niedźwiecki <hello@uxu.pl>
2022-04-20 00:53:41 +0200

    update

--- next.config.js
+++ next.config.js
@@ -1,19 +1,27 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  productionBrowserSourceMaps: true,
-  images: {
-    domains: ["s3.eu-central-1.amazonaws.com"],
-    formats: ["image/avif", "image/webp"],
-  },
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack"],
-    });
+const removeImports = require("next-remove-imports")();
 
-    return config;
-  },
+module.exports = (phase, { defaultConfig }) => {
+  /**
+   * @type {import('next').NextConfig}
+   */
+  const nextConfig = {
+    reactStrictMode: true,
+    productionBrowserSourceMaps: true,
+    images: {
+      domains: ["s3.eu-central-1.amazonaws.com"],
+      formats: ["image/avif", "image/webp"],
+    },
+    webpack(config) {
+      config.module.rules.push({
+        test: /\.svg$/,
+        use: ["@svgr/webpack"],
+      });
+
+      return config;
+    },
+  };
+
+  return removeImports({
+    ...nextConfig,
+  });
 };
-
-module.exports = nextConfig;
--- src/components/templates/section/dasbordUserAddArticle/component.section.dasbordUserAddArticle.index.tsx
+++ src/components/templates/section/dasbordUserAddArticle/component.section.dasbordUserAddArticle.index.tsx
@@ -1,4 +1,3 @@
-import { Session } from "inspector";
 import { useForm } from "react-hook-form";
 import { useEffect, useState } from "react";
 import { useSession } from "next-auth/react";
@@ -6,11 +5,27 @@ import { Container, Row, Col } from "components/orgamis/flexboxgrid/index.flexbo
 import { Input, TextArea, CheckBox, enumInputType } from "components/molecules/form/component.form.index";
 import { Section, Header, Title } from "./component.section.dasbordUserAddArticle.style";
 
+import "@uiw/react-md-editor/markdown-editor.css";
+import "@uiw/react-markdown-preview/markdown.css";
+import dynamic from "next/dynamic";
+
+const MDEditor = dynamic<any>((): any => import("@uiw/react-md-editor").then((mod) => mod.default), { ssr: false });
+
+const EditerMarkdown = dynamic<any>(
+  (): any =>
+    import("@uiw/react-md-editor").then((mod) => {
+      return mod.default.Markdown;
+    }),
+  { ssr: false }
+);
+
+const Markdown = dynamic<any>((): any => import("@uiw/react-markdown-preview").then((mod) => mod.default), { ssr: false });
+
 export default function SectionDasbordAddArticle({ data: { title } }: { data: { title: string } }) {
   const { data: session, status } = useSession();
   const [type, setType] = useState("a");
-
   const [addArticle, setAddArticle] = useState(true);
+  const [value, setValue] = useState("**Hello world!!!**");
 
   const {
     watch,
@@ -35,6 +50,15 @@ export default function SectionDasbordAddArticle({ data: { title } }: { data: { 
           </Col>
           <Col xs={12}>
             <Input id="title" name="title" type={enumInputType.text} error={errors.title} placeholder="Tytuł wpisu" defaultValue={undefined} register={register} required />
+          </Col>
+          <Col xs={12}>
+            <div data-color-mode="dark">
+              <MDEditor value={value} onChange={setValue} />
+              <div style={{ paddingTop: 50 }}>
+                <Markdown source={value} />
+              </div>
+              <EditerMarkdown source={value} />
+            </div>
           </Col>
         </Row>
       </Container>
