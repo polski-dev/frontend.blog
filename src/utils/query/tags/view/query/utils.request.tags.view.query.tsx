9dbe8a41f62f98876561ce7f18b8a02933b243ed
Paweł Niedźwiecki <hello@uxu.pl>
2022-06-29 14:35:41 +0200

    i update request in tags views

--- src/utils/query/tags/view/query/utils.request.tags.view.query.tsx
+++ src/utils/query/tags/view/query/utils.request.tags.view.query.tsx
@@ -1,35 +1,43 @@
-import axios, { AxiosResponse } from "axios";
+import axios, { AxiosResponse, AxiosError } from "axios";
+import { MessageErrorInAPI } from "utils/messages/utils.messages.errors";
 import { ViewAddInTagType } from "../types/utils.request.tags.view.types";
 
 export async function viewAddInTagBackEnd({ tagId }: { tagId?: number }): Promise<ViewAddInTagType> {
-  if (!tagId)
-    return {
-      data: null,
-      error: {
-        status: 400,
-        name: "Wrong field",
-        message: "Wrong one from fields",
-        details: {},
-      },
-    };
+  if (!tagId) return MessageErrorInAPI({ name: "Wrong field", message: "Wrong one from fields" });
 
-  const res: AxiosResponse<ViewAddInTagType> = await axios.put(process.env.BACKEND_API_URL + `/api/tag/views/${tagId}`);
-  return res.data;
+  let data = {};
+  try {
+    const res: AxiosResponse<ViewAddInTagType> = await axios.put(process.env.BACKEND_API_URL + `/api/tag/views/${tagId}`);
+
+    data = res.data;
+  } catch (err) {
+    if (axios.isAxiosError(err)) {
+      const serverError = err as AxiosError<ViewAddInTagType>;
+      if (serverError && serverError.response) {
+        data = serverError.response.data;
+      }
+    } else data = { data: null };
+  }
+
+  return data;
 }
 
 export async function viewAddInTagFrontEnd({ tagId }: { tagId?: number }): Promise<ViewAddInTagType> {
-  if (!tagId)
-    return {
-      data: null,
-      error: {
-        status: 400,
-        name: "Wrong field",
-        message: "Wrong one from fields",
-        details: {},
-      },
-    };
+  if (!tagId) return MessageErrorInAPI({ name: "Wrong field", message: "Wrong one from fields" });
 
-  const res: AxiosResponse<ViewAddInTagType> = await axios.put(`/api/tag/view/${tagId}`);
+  let data = {};
+  try {
+    const res: AxiosResponse<ViewAddInTagType> = await axios.put(`/api/tag/view/${tagId}`);
 
-  return res.data;
+    data = res.data;
+  } catch (err) {
+    if (axios.isAxiosError(err)) {
+      const serverError = err as AxiosError<ViewAddInTagType>;
+      if (serverError && serverError.response) {
+        data = serverError.response.data;
+      }
+    } else data = { data: null };
+  }
+
+  return data;
 }
