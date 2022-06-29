202dae9421b4bb8112309d3671771a25617f358d
Paweł Niedźwiecki <hello@uxu.pl>
2022-06-29 18:38:28 +0200

    i added query for authUserSingUp

--- src/utils/query/auth/query/utils.request.auth.user.query.tsx
+++ src/utils/query/auth/query/utils.request.auth.user.query.tsx
@@ -36,3 +36,39 @@ export async function authUserSingInFrontEnd({ identifier, password }: { identif
 
   return data;
 }
+
+export async function authUserSingUpBackEnd({ username, identifier, password }: { username: string; identifier: string; password: number }): Promise<AuthSingInUserType> {
+  let data = {};
+  try {
+    const res: AxiosResponse<AuthSingInUserDataType> = await axios.post(process.env.BACKEND_API_URL + `/api/auth/local/register`, { username, identifier, password });
+
+    data = { data: res.data };
+  } catch (err) {
+    if (axios.isAxiosError(err)) {
+      const serverError = err as AxiosError<AuthSingInUserDataType>;
+      if (serverError && serverError.response) {
+        data = serverError.response.data;
+      }
+    } else data = { data: null };
+  }
+
+  return data;
+}
+
+export async function authUserSingUpFrontEnd({ username, identifier, password }: { username: string; identifier: string; password: number }): Promise<AuthSingInUserType> {
+  let data = {};
+  try {
+    const res: AxiosResponse<AuthSingInUserType> = await axios.post(`/api/auth/local/register`, { username, identifier, password });
+
+    data = res.data;
+  } catch (err) {
+    if (axios.isAxiosError(err)) {
+      const serverError = err as AxiosError<AuthSingInUserType>;
+      if (serverError && serverError.response) {
+        data = serverError.response.data;
+      }
+    } else data = { data: null };
+  }
+
+  return data;
+}
