f0ad253ca1668495ca0d42fb2b279a27233493cb
Paweł Niedźwiecki <hello@uxu.pl>
2022-06-29 13:35:58 +0200

    i update query for subscribe

--- src/utils/query/subscribe/users/query/utils.request.user.subscribe.query.tsx
+++ src/utils/query/subscribe/users/query/utils.request.user.subscribe.query.tsx
@@ -1,27 +1,83 @@
-import axios, { AxiosResponse } from "axios";
+import axios, { AxiosResponse, AxiosError } from "axios";
 import { MessageErrorInAPI } from "utils/messages/utils.messages.errors";
 import { UserAmISubscribeType, UserChangeSubscribeType } from "../types/utils.request.user.subscribe.types";
 
 export async function userAmISubscribeBackEnd({ userId, authToken }: { userId?: number; authToken: string }): Promise<UserAmISubscribeType> {
   if (!userId || !authToken) return MessageErrorInAPI({ name: "Wrong field", message: "Wrong someone fields userId, authToken" });
-  const amISubscribeStatus: AxiosResponse<UserAmISubscribeType> = await axios.get(process.env.BACKEND_API_URL + `/api/subscribes/users/${userId}`, { headers: { Authorization: authToken } });
-  return amISubscribeStatus.data;
+
+  let data = {};
+  try {
+    const res: AxiosResponse<UserAmISubscribeType> = await axios.get(process.env.BACKEND_API_URL + `/api/subscribes/users/${userId}`, { headers: { Authorization: authToken } });
+
+    data = res.data;
+  } catch (err) {
+    if (axios.isAxiosError(err)) {
+      const serverError = err as AxiosError<UserAmISubscribeType>;
+      if (serverError && serverError.response) {
+        data = serverError.response.data;
+      }
+    } else data = { data: null };
+  }
+
+  return data;
 }
 
 export async function userAmISubscribeFrontEnd({ userId, authToken }: { userId?: number; authToken?: string }): Promise<UserAmISubscribeType> {
   if (!userId || !authToken) return MessageErrorInAPI({ name: "Wrong field", message: "Wrong someone fields userId, authToken" });
-  const amISubscribeStatus: AxiosResponse<UserAmISubscribeType> = await axios.get(`/api/subscribe/user/${userId}`, { headers: { Authorization: `Bearer ${authToken}` } });
-  return amISubscribeStatus.data;
+
+  let data = {};
+  try {
+    const res: AxiosResponse<UserAmISubscribeType> = await axios.get(`/api/subscribe/user/${userId}`, { headers: { Authorization: `Bearer ${authToken}` } });
+
+    data = res.data;
+  } catch (err) {
+    if (axios.isAxiosError(err)) {
+      const serverError = err as AxiosError<UserAmISubscribeType>;
+      if (serverError && serverError.response) {
+        data = serverError.response.data;
+      }
+    } else data = { data: null };
+  }
+
+  return data;
 }
 
 export async function userChangeSubscribeBackEnd({ userId, authToken }: { userId?: number; authToken: string }): Promise<UserChangeSubscribeType> {
   if (!userId || !authToken) return MessageErrorInAPI({ name: "Wrong field", message: "Wrong someone fields userId, authToken" });
-  const amISubscribeStatus: AxiosResponse<UserChangeSubscribeType> = await axios.put(process.env.BACKEND_API_URL + `/api/subscribes/users/${userId}`, {}, { headers: { Authorization: authToken } });
-  return amISubscribeStatus.data;
+
+  let data = {};
+  try {
+    const res: AxiosResponse<UserChangeSubscribeType> = await axios.put(process.env.BACKEND_API_URL + `/api/subscribes/users/${userId}`, {}, { headers: { Authorization: authToken } });
+
+    data = res.data;
+  } catch (err) {
+    if (axios.isAxiosError(err)) {
+      const serverError = err as AxiosError<UserChangeSubscribeType>;
+      if (serverError && serverError.response) {
+        data = serverError.response.data;
+      }
+    } else data = { data: null };
+  }
+
+  return data;
 }
 
 export async function userChangeSubscribeFrontEnd({ userId, authToken }: { userId?: number; authToken?: string }): Promise<UserChangeSubscribeType> {
   if (!userId || !authToken) return MessageErrorInAPI({ name: "Wrong field", message: "Wrong someone fields userId, authToken" });
-  const amISubscribeStatus: AxiosResponse<UserChangeSubscribeType> = await axios.put(`/api/subscribe/user/${userId}`, {}, { headers: { Authorization: `Bearer ${authToken}` } });
-  return amISubscribeStatus.data;
+
+  let data = {};
+  try {
+    const res: AxiosResponse<UserChangeSubscribeType> = await axios.put(`/api/subscribe/user/${userId}`, {}, { headers: { Authorization: `Bearer ${authToken}` } });
+
+    data = res.data;
+  } catch (err) {
+    if (axios.isAxiosError(err)) {
+      const serverError = err as AxiosError<UserChangeSubscribeType>;
+      if (serverError && serverError.response) {
+        data = serverError.response.data;
+      }
+    } else data = { data: null };
+  }
+
+  return data;
 }
