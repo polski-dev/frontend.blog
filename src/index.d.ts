cacbfa6bd2083f74a0a94c145873521c502d4d6a
Paweł Niedźwiecki <hello@uxu.pl>
2022-06-29 20:08:47 +0200

    update index.d.ts

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,7 +1,6 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
-      NEXT_PUBLIC_API_URL: string;
       JWT_SECRET: string;
       NODE_ENV: "development" | "production";
       PORT?: string;
