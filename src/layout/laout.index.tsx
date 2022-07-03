9b134357a001756df7936ee12e959098b682d62d
Paweł Niedźwiecki <hello@uxu.pl>
2022-07-03 18:15:44 +0200

    i added tag tiwh google analitycs

--- src/layout/laout.index.tsx
+++ src/layout/laout.index.tsx
@@ -1,14 +1,20 @@
-import Head from "next/head";
+import Script from "next/script";
 import Header from "components/templates/header/index.header";
 import Footer from "components/templates/footer/index.footer";
 import PopupWithAllowCookies from "components/templates/popupWithAllowCookies/component.popupWithAllowCookies.index";
 
-export default function Laout({ children }: any) {
+export default function Laout({ children }: any): JSX.Element {
   return (
     <>
-      <Head>
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4518094843351143" type="module" />
-      </Head>
+      <Script src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4518094843351143" strategy="afterInteractive" />
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-XKSJ3VXXQW" strategy="afterInteractive" />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-XKSJ3VXXQW');`}
+      </Script>
+
       <Header />
       {children}
       <Footer />
