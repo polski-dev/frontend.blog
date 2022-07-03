797f3e9745766fc37f640e6776bf5afe8c7ad6ef
Paweł Niedźwiecki <hello@uxu.pl>
2022-07-03 17:12:30 +0200

    i added  tag for google adsence

--- src/layout/laout.index.tsx
+++ src/layout/laout.index.tsx
@@ -1,9 +1,13 @@
+import Head from "next/head";
 import Header from "components/templates/header/index.header";
 import Footer from "components/templates/footer/index.footer";
 
 export default function Laout({ children }: any) {
   return (
     <>
+      <Head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4518094843351143" type="module" />
+      </Head>
       <Header />
       {children}
       <Footer />
